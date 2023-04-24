efc1670dcd60d96a584a6dff45cb1b778541ea53
Elijah Barreto <51147388+ElijahBcodes@users.noreply.github.com>
2023-04-24 09:23:25 -0700

    Log for homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -18,7 +18,7 @@ export default function Home() {
   <div className="hero-overlay bg-opacity-1000"></div>
   <div className="hero-content text-center text-neutral-content">
     <div className="max-w-md">
-      <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
+      <h1 className="mb-5 text-5xl font-bold">Welcome</h1>
       <p className="mb-5">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
       <button className="btn btn-primary">Get Started</button>
     </div>
